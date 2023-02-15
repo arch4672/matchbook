57113b364f084b8a7965e08fd3fc608113073329
Chris Archer <arch4672@gmail.com>
2023-02-15 15:02:47 +0000

    Format with black

--- matchbook/endpoints/keepalive.py
+++ matchbook/endpoints/keepalive.py
@@ -1,4 +1,3 @@
-
 import json
 
 from matchbook.endpoints.baseendpoint import BaseEndpoint
@@ -6,23 +5,38 @@ from matchbook.exceptions import AuthError
 
 
 class KeepAlive(BaseEndpoint):
-
     def __call__(self, session=None):
         session = session or self.client.session
-        response = self.request('GET', self.client.urn_main, 'security/session', data=self.data, session=session)
+        response = self.request(
+            "GET",
+            self.client.urn_main,
+            "security/session",
+            data=self.data,
+            session=session,
+        )
         if response.status_code == 200:
             pass
         elif response.status_code == 401:
-            response = self.request("POST", self.client.urn_main, 'security/session', data=self.data, session=session)
+            response = self.request(
+                "POST",
+                self.client.urn_main,
+                "security/session",
+                data=self.data,
+                session=session,
+            )
             if response.status_code == 200:
                 response_json = response.json()
-                self.client.set_session_token(response_json.get('session-token'), response_json.get('user-id'))
+                self.client.set_session_token(
+                    response_json.get("session-token"), response_json.get("user-id")
+                )
             else:
                 raise AuthError(response)
         else:
             raise AuthError(response)
 
-    def request(self, request_method, urn, method, params={}, data={}, target=None, session=None):
+    def request(
+        self, request_method, urn, method, params={}, data={}, target=None, session=None
+    ):
         """
         :param request_method: type of request to be sent.
         :param urn: matchbook urn to append to url specified.
@@ -34,14 +48,17 @@ class KeepAlive(BaseEndpoint):
         :param session: Requests session to be used, reduces latency.
         """
         session = session or self.client.session
-        data['session-token'] = self.client.session_token
-        data['user-id'] = self.client.user_id
-        request_url = '%s%s%s' % (self.client.url, urn, method)
-        response = session.request(
-            request_method, request_url, params=params, data=json.dumps(data), headers=self.client.headers
+        data["session-token"] = self.client.session_token
+        data["user-id"] = self.client.user_id
+        request_url = f"{self.client.url}{urn}{method}"
+        return session.request(
+            request_method,
+            request_url,
+            params=params,
+            data=json.dumps(data),
+            headers=self.client.headers,
         )
-        return response
 
     @property
     def data(self):
-        return {'username': self.client.username, 'password': self.client.password}
+        return {"username": self.client.username, "password": self.client.password}
